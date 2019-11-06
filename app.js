9917dd22cc7f69233d1b356b8d9b91d8b0b88c7a
Prasham Jain <prshmjain1@gmail.com>
2019-11-06 18:45:40 +0530

    Instantiated the application through app.js"

---
+++ app.js
@@ -0,0 +1,24 @@
+//importing express from node modules
+var express = require('express');
+
+//importing todoController
+var todoController = require('./controllers/todoController');
+
+//creating constructor/instance for express for using various methods of express
+var app = express();
+
+//setting up template engine using ejs
+app.set('view engine', 'ejs');
+
+//express static file middleware to access styles
+app.use(express.static('./assets'));
+
+//fire controllers
+todoController(app);       //passing app instance of express to todoController
+
+//listen to port
+app.listen(3000);
+console.log("You're listening to port 3000");
+
+
+
---
+++ controllers/todoController.js
@@ -0,0 +1,17 @@
+module.exports = function(app) {    //creating a function with express instance argument
+    
+    //when a url request is made to todo
+    app.get('/todo', function (req, res) {
+        
+    });
+
+    //when a user wants to add a new todo 
+    app.post('/todo', function (req, res) {
+
+    });
+
+    //when a user wants to delete a todo
+    app.delete('/todo', function (req, res) {
+
+    });
+};
